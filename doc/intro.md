5d6e3aba3eb6935ccc1c062e74ea6421e2e9593f
Eugene Hardbread <agnivalent@gmail.com>
2017-12-02 16:23:09 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tetrad
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tetrad/core_test.clj
@@ -0,0 +1,7 @@
+(ns tetrad.core-test
+  (:require [clojure.test :refer :all]
+            [tetrad.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
